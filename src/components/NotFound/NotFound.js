7de8e122957be3e9bdeb957f010337f437fbe62b
monibBormon <monibbormon14@gmail.com>
2021-10-04 22:39:56 +0600

    404 update

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h2 className='text-6xl bg-blue-500 py-5 mb-2 text-center text-white font-semibold capitalize'>404 page not Found!</h2>
+            <h2 className='text-6xl bg-blue-500 py-24 mb-2 text-center text-white font-semibold capitalize'>404 page not Found!</h2>
         </div>
     );
 };
